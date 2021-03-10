60bd3f916aced697f245ee468d923093f658cdd2
Andre LaFleur <cincospenguinos@gmail.com>
2021-03-09 17:32:23 -0700

    Move some files

--- Assets/Scripts/Runes/Rune.cs
+++ Assets/Scripts/Runes/Rune.cs
@@ -1,26 +1,3 @@
-public class RuneTrigger {
-  private string _eventName;
-  private int _enemiesDied;
-
-  public bool IsTriggered => _enemiesDied % 3 == 0;
-
-  public RuneTrigger(string eventName) {
-    _enemiesDied = 0;
-    _eventName = eventName;
-  }
-
-  public bool OnEvent(string name) {
-    if (_eventName == name) {
-      _enemiesDied += 1;
-    }
-
-    return IsTriggered;
-  }
-
-  /// Not needed for this specific trigger...
-  public void Reset() {}
-}
-
 public class RuneAction {
   private Entity _entity;
 
