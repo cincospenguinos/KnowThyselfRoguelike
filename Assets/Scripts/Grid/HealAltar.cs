72b7aa45706f4b0c3f011d36e3fd94506e9ea389
Andre LaFleur <cincospenguinos@gmail.com>
2021-03-12 22:28:13 -0700

    Extract a super class

--- Assets/Scripts/Grid/HealAltar.cs
+++ Assets/Scripts/Grid/HealAltar.cs
@@ -1,11 +1,38 @@
 using System;
 using UnityEngine;
 
-public class RuneEditAltar : Entity {
+public abstract class Altar : Entity {
   public event Action OnUsed;
   public bool Used = false;
 
-  public RuneEditAltar(Vector2Int coords) : base(coords, 100) {}
+  public Altar(Vector2Int coords) : base(coords, 100) {}
+
+   public override void onWalkInto(Player player) {
+    if (!Used) {
+      Used = true;
+      Use(player);
+    }
+  }
+
+  protected void TriggerOnUse() {
+    OnUsed?.Invoke();
+  }
+
+  public abstract void Use(Player player);
+}
+
+public class RuneEditAltar : Altar {
+  public RuneEditAltar(Vector2Int coords) : base(coords) {}
+
+  public override void Use(Player player) {
+    TriggerOnUse();
+    Debug.Log("Hey show the screen homie");
+    // TODO: Show the screen
+  }
+}
+
+public class HealAltar : Altar {
+  public HealAltar(Vector2Int coords) : base(coords) {}
 
   public override void onWalkInto(Player player) {
     if (!Used) {
@@ -14,28 +41,8 @@ public class RuneEditAltar : Entity {
     }
   }
 
-  void Use(Player player) {
-    OnUsed?.Invoke();
-    Debug.Log("Hey show the screen homie");
-    // TODO: Show the screen
-  }
-}
-
-public class HealAltar : Entity {
-  public event Action OnUsed;
-  public bool used = false;
-  public HealAltar(Vector2Int coords) : base(coords, 100) {
-  }
-
-  public override void onWalkInto(Player player) {
-    if (!used) {
-      used = true;
-      Use(player);
-    }
-  }
-
-  void Use(Player player) {
-    OnUsed?.Invoke();
+  public override void Use(Player player) {
+    TriggerOnUse();
     player.Heal(player.MaxHitPoints - player.HitPoints);
   }
 }
