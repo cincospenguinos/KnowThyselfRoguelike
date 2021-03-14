7647ebe70f6aa17b59788fd5f3bd74c1b8dd0eb8
Andre LaFleur <cincospenguinos@gmail.com>
2021-03-14 16:28:23 -0600

    Clicking the accept button hides the accept button

--- Assets/RuneEditorAcceptButtonManager.cs
+++ Assets/RuneEditorAcceptButtonManager.cs
@@ -7,7 +7,9 @@ public class RuneEditorAcceptButtonManager : MonoBehaviour
     public GameObject Button;
 
     // Start is called before the first frame update
-    void Start() {}
+    void Start() {
+        // ((Button) Button)
+    }
 
     // Update is called once per frame
     void Update()
