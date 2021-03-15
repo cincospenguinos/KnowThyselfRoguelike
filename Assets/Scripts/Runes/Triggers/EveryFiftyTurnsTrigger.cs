0fddcc47b338e961f17a0dbb2c8fc8e2b2bd70f3
Xiaohan Zhang <hellocharlien@hotmail.com>
2021-03-14 19:58:01 -0700

    three variants in triggers

--- Assets/Scripts/Runes/Triggers/EveryFiftyTurnsTrigger.cs
+++ Assets/Scripts/Runes/Triggers/EveryFiftyTurnsTrigger.cs
@@ -1,14 +1,10 @@
-public class EveryFiftyTurnsTrigger : RuneTrigger {
-  public override int ChargeBase => 50;
+public abstract class EveryXTurnsTrigger : RuneTrigger {
+  public abstract int Tempo { get; }
   private int _elapsedTurns;
-  public bool IsTriggered => _elapsedTurns > 0 && _elapsedTurns % 50 == 0;
+  public bool IsTriggered => _elapsedTurns > 0 && _elapsedTurns % Tempo == 0;
 
-  public EveryFiftyTurnsTrigger(Entity e) : base(e) {
+  public EveryXTurnsTrigger(Entity e) : base(e) {
       _elapsedTurns = 0;
-  }
-
-  public override RuneTrigger Clone() {
-    return new EveryFiftyTurnsTrigger(OwningEntity);
   }
 
   public override bool ShouldCharge(GameEvent gameEvent) {
@@ -19,9 +15,42 @@ public class EveryFiftyTurnsTrigger : RuneTrigger {
     return IsTriggered;
   }
 
-  public override string Text() => $"in <b>{50 - _elapsedTurns}</b> turns.";
+  public override string Text() => $"in <b>{Tempo - _elapsedTurns}</b> turns.";
 
   public override void Reset() {
     _elapsedTurns = 0;
+  }
+}
+
+public class Every50TurnsTrigger : EveryXTurnsTrigger {
+  public Every50TurnsTrigger(Entity e) : base(e) { }
+
+  public override int ChargeBase => 50;
+  public override int Tempo => 50;
+
+  public override RuneTrigger Clone() {
+    return new Every50TurnsTrigger(OwningEntity);
+  }
+}
+
+public class Every100TurnsTrigger : EveryXTurnsTrigger {
+  public Every100TurnsTrigger(Entity e) : base(e) { }
+
+  public override int ChargeBase => 110;
+  public override int Tempo => 100;
+
+  public override RuneTrigger Clone() {
+    return new Every100TurnsTrigger(OwningEntity);
+  }
+}
+
+public class Every200TurnsTrigger : EveryXTurnsTrigger {
+  public Every200TurnsTrigger(Entity e) : base(e) { }
+
+  public override int ChargeBase => 225;
+  public override int Tempo => 200;
+
+  public override RuneTrigger Clone() {
+    return new Every200TurnsTrigger(OwningEntity);
   }
 }
