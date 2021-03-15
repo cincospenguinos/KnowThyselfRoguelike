e64e3bffccd94c5fd493caf6de7ce97b431c0f4c
Andre LaFleur <cincospenguinos@gmail.com>
2021-03-14 20:51:13 -0600

    Add cooldown

--- Assets/Scripts/Runes/Triggers/HalfHitPointsTrigger.cs
+++ Assets/Scripts/Runes/Triggers/HalfHitPointsTrigger.cs
@@ -1,5 +1,6 @@
 public class HalfHitPointsTrigger : RuneTrigger {
   public override int ChargeBase => 200;
+  public int Cooldown = 100;
 
   public HalfHitPointsTrigger(Entity e) : base(e) {}
 
@@ -8,11 +9,15 @@ public class HalfHitPointsTrigger : RuneTrigger {
   }
 
   public override bool ShouldCharge(GameEvent gameEvent) {
-    bool ownerCrossedThreshold = gameEvent.GameEventType == GameEvent.EventType.REACH_HALF_HIT_POINTS && FromOwnEntity(gameEvent); 
+    if (gameEvent.GameEventType == GameEvent.EventType.TURN_ELAPSED && Cooldown < 100) {
+      Cooldown += 1;
+    }
+
+    bool ownerCrossedThreshold = gameEvent.GameEventType == GameEvent.EventType.REACH_HALF_HIT_POINTS && FromOwnEntity(gameEvent) && Cooldown == 100; 
     return ownerCrossedThreshold;
   }
 
-  public override string Text() => $"when your HP falls below (<b>{(int)(OwningEntity.MaxHitPoints / 2)}</b>).";
+  public override string Text() => $"when your HP falls below (<b>{(int)(OwningEntity.MaxHitPoints / 2)}</b>, {Cooldown} cooldown).";
 
   public override void Reset() {}
 }
